e746a1a4ba3f3eb2434d9beae7045f2fbadbc384
Devansh Mehta <devansh.mht@gmail.com>
2013-11-13 00:22:59 -0500

    adding section properties map

--- Config.java
+++ Config.java
@@ -1,5 +1,17 @@
 package config; 
 
-public class Config {
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
 
+public class Config {
+  
+    public Config(String fileName) {
+        this.fileName = fileName;
+        sectionPropeties = new HashMap<String, Properties>();
+    }
+  
+    private Map<String, Properties> sectionProperties;
+    private String fileName;
 }
