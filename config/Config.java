20f88e1cf7417561257bdb980d5234e3b897719e
Devansh Mehta <devansh.mht@gmail.com>
2013-11-13 00:35:01 -0500

    adding class level comments

--- config/Config.java
+++ config/Config.java
@@ -5,6 +5,14 @@ import java.util.Properties;
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+ * Parses the configuration file. The configuration file
+ * either contains section name or properties. The properties
+ * should come after the section name. Line starting with 
+ * # is considered to be comment.
+ * 
+ * @ author devansh.mht@gmail.com
+ */
 public class Config {
   
     public Config(String fileName) {
