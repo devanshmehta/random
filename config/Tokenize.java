482c8b4850fe92df863f4dedebdd92a53a994144
Devansh Mehta <devansh.mht@gmail.com>
2013-11-13 10:45:52 -0500

    Adding tokens used during tokenizing.

--- config/Tokenize.java
+++ config/Tokenize.java
@@ -3,3 +3,27 @@ package config;
 public class Tokenize {
 
 }
+
+enum TOKEN_TYPE {
+    COMMENT, PROPERTY, SECTION
+}
+
+class Token {
+    
+    public Token(TOKEN_TYPE tokenType, String value) {
+        this.tokenType = tokenType;
+        this.value = value;
+    }
+    
+    public TOKEN_TYPE getTokenType() {
+        return tokenType;
+    }
+    
+    public String getValue() {
+        return value;
+    }
+    
+    private String TOKEN_TYPE tokenType;
+    private String value;
+  
+}
