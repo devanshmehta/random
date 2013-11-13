7383b0082f340e69a3b22db6362560ec90c4e4cd
Devansh Mehta <devansh.mht@gmail.com>
2013-11-13 13:21:11 -0500

    Adding a static method to get a token for the line.

--- config/Tokenize.java
+++ config/Tokenize.java
@@ -1,7 +1,21 @@
 package config;
 
 public class Tokenize {
-
+    
+    /**
+     * Tokenizes a line into token
+     * 
+     * @param line
+     * @return Token if found otherwise null
+     * @throws NullPointerException if the line is null or 
+     *         has zero length.
+     */
+    public static Token getTokenFor(String line) {
+        if(line == null || line.length() == 0) {
+            throw new NullPointerException();
+        }
+    }
+    
 }
 
 enum TOKEN_TYPE {
