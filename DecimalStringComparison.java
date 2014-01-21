dcf2c55ba8ea896f772ac4858e6aabc003ea941d
Devansh Mehta <devansh.mht@gmail.com>
2014-01-21 10:14:01 -0500

    comparison for only integers.

--- DecimalStringComparison.java
+++ DecimalStringComparison.java
@@ -1,3 +1,43 @@
-public class DecimalStringComparison implements Comparable {
+import java.util.Comparator;
 
+/**
+ * Compares decimals represented as String.
+ * 
+ * @author devansh.mht@gmail.com (Devansh Mehta)
+ */
+public class DecimalStringComparison implements Comparator<String> {
+
+    public int compareTo(String firstNum, String secondNum) {
+        int charsBeforeDotInFirstNum = getCharsBeforeDot(firstNum);
+        int charsBeforeDotInSecondNum = getCharsBeforeDot(secondNum);
+        if(charsBeforeDotInFirstNum != charsBeforeDotInSecondNum) {
+            return charsBeforeDotInFirstNum - charsBeforeDotInSecondNum;
+        }
+        for(int i = 0; i < charsBeforeDotInFirstNum; ++i) {
+            int firstNumDigit = firstNum.charAt(i) - '0';
+            int secondNumDigit = secondNum.charAt(i) - '0';
+            if(firstNumDigit != secondNumDigit) {
+                return firstNumDigit - secondNumDigit;
+            }
+        }
+        //It means everything before the dot is similar.
+        return 0;
+    }
+    
+    /**
+     * Returns the number of characters before the decimal 
+     * point starts.
+     * 
+     * @return number of characters before decimal point start.
+     */
+    private int getCharsBeforeDot(String num) {
+        int charsBeforeDot = 0;
+        for(int i = 0; i < num.length(); ++i) {
+            if(num.charAt(i) == '.') {
+                break;
+            }
+            ++charsBeforeDot;
+        }
+        return charsBeforeDot;
+    }
 }
