1fa089646dc8c691c58d7b1a68b1fb4829b217e5
Devansh Mehta <devansh.mht@gmail.com>
2014-02-11 11:22:05 -0500

    implementation for lsearch

--- interval_find.py
+++ interval_find.py
@@ -15,5 +15,26 @@ def rsearch(l, val, lo = 0, high = None):
                 lo = mid + 1
             else:
                 return mid
-    return lo        
+    return lo
+    
+def lsearch(l, val, lo = 0, high = None):
+    """returns the left most index of val if val
+       is in l otherwise returns index x such that
+       l[x] <= val and l[x + 1] > val"""
+       if not high:
+           high = len(l) - 1
+        while lo < high:
+            mid = (lo + high) // 2
+            if (lo + high) %2 != 0:
+                mid += 1
+            if l[mid] < val: 
+                lo = mid
+            elif l[mid] > val:
+                high = mid - 1
+            else:
+                if l[mid - 1] == val:
+                    high = mid - 1
+                else:
+                    return mid
+        return lo
 
