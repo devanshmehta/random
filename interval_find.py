d895efca1b9679b0d1cb25ed6afd364710daa0f4
Devansh Mehta <devansh.mht@gmail.com>
2014-02-11 11:34:03 -0500

    implementation for find_interval

--- interval_find.py
+++ interval_find.py
@@ -21,8 +21,8 @@ def lsearch(l, val, lo = 0, high = None):
     """returns the left most index of val if val
        is in l otherwise returns index x such that
        l[x] <= val and l[x + 1] > val"""
-       if not high:
-           high = len(l) - 1
+        if not high:
+            high = len(l) - 1
         while lo < high:
             mid = (lo + high) // 2
             if (lo + high) %2 != 0:
@@ -37,4 +37,22 @@ def lsearch(l, val, lo = 0, high = None):
                 else:
                     return mid
         return lo
+        
+def find_interval(l, l_value, r_value):
+    """returns (lindex, rindex) from l such that
+       l[lindex] <=  l_value <= r_value <= l[rindex]"""
+        l_r = rsearch(l, l_value)
+        l_l = lsearch(l, l_value)
+        r_r = rsearch(;, r_value)
+        r_l = lsearch(l, r_value)
+        if l[l_l] == l[l_r]:
+            l_index = l_1
+        else:
+            l_index = l_r
+        if l[r_r] == l[r_l]
+            r_index = r_r
+        else:
+            r_index = r_l
+        return (l_index, r_index)
+       
 
