ee9d5e9ce836f94954033b9a8a408499e5f5dc14
Devansh Mehta <devansh.mht@gmail.com>
2014-03-31 17:17:33 -0400

    adding methods to user CircularBuffer

--- CircularBuffer.java
+++ CircularBuffer.java
@@ -4,11 +4,50 @@
 * 
 * @author devansh.mht@gmail.com
 */
-public class CircularBuffer {
+public class CircularBuffer <E>{
     
     public CircularBuffer(int size) {
         this.size = size;
+        arr = new E[size];
+        count = 0;
+        addPointer = 0;
+        getPointer = 0;
+    }
+    
+    public boolean isFull() {
+        int diff = addPointer - getPointer;
+        if (diff < 0) {
+            diff *= -1;
+        }
+        return diff == 1 || diff == size;
+    }
+    
+    public boolean isEmpty() {
+        return addPointer == getPointer;
+    }
+    
+    public void add(E e) {
+        if (isFull()) {
+            getPointer++;
+        }
+        int add = addPointer % size;
+        arr[add] = e
+        ++addPointer;
+    }
+    
+    public E poll() {
+        if (isEmpty()) {
+            return null;
+        }
+        int get = getPointer % size;
+        E e = arr[get];
+        ++getPointer;
+        return e;
     }
     
     private int size;
+    private int getPointer;
+    private int addPointer;
+    private int count;
+    private E[] arr;
 }
