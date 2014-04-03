1a1a6c92001e49f9993423c9ead0ea7bda55ba50
Devansh Mehta <devansh.mht@gmail.com>
2014-04-03 12:26:50 -0400

    adding methods to solve the problem

--- nqueens.py
+++ nqueens.py
@@ -1,3 +1,12 @@
+def count_queens_starting_at(row, column):
+    pass
+
+def count_queens(n):
+    count = 0
+    for i in xrange(n):
+        count += count_queens_starting_at(i, 0)
+    return count
+    
 def main():
     n = int(raw_input())
     
