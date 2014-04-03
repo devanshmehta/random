a4d6484c6e75f3ce357251769e344d8e7e86a249
Devansh Mehta <devansh.mht@gmail.com>
2014-04-03 14:57:30 -0400

    program does not work

--- nqueens.py
+++ nqueens.py
@@ -1,14 +1,56 @@
-def count_queens_starting_at(row, column):
-    pass
+'''counting all the solutions in the n queens problem.
+   The solution is based on niklaus wirth's solution
+   for 8 queens board
+'''
+___author__ = 'devansh.mht@gmail.com'
 
+class Board(object):
+    '''board class to be userd for blocking and unblocking'''
+    
+    def __init__(self, n):
+        self.n = n
+        self.rows = [True] * n
+        self.sum_diag = [True] * ((n * 2) - 1)
+        self.diff_diag = [True] * ((n * 2) - 1)
+    
+    def block_board(self, i, j):
+        self.rows[i] = False
+        self.sum_diag[i + j] = False
+        self.diff_diag[i - j + (self.n - 1)] = False
+
+    def unblock_board(self, i, j):
+        self.rows[i] = True
+        self.sum_diag[i + j] = True
+        self.diff_diag[i - j + (self.n - 1)] = True
+        
+    def safe_square(self, i, j):
+        (self.rows[i] and self.sum_diag[i + j] and 
+         self.diff_diag[i - j + (self.n - 1)])
+
+def count_queens_starting_at(row, column, board):
+    count = 0
+    if column >= board.n:
+		return 1
+    for j in xrange(column, board.n):
+        for i in xrange(row, board.n):
+            if not board.safe_square(i, j):   
+                continue
+            board.block_board(i, j)
+            count += count_queens_starting_at(i, j, board)
+            board.unblock_board(i, j)
+    return count
+    
 def count_queens(n):
     count = 0
     for i in xrange(n):
-        count += count_queens_starting_at(i, 0)
+        for j in xrange(n):
+            board = Board(n)
+            count += count_queens_starting_at(i, j, board)
     return count
     
 def main():
     n = int(raw_input())
-    
+    print count_queens(n)
+
 if __name__ == '__main__':
     main()
