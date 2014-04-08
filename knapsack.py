7e0c4ffc0bf4062f2ae873d58cd876289471ea49
Devansh Mehta <devansh.mht@gmail.com>
2014-04-08 00:14:59 -0400

    fixed syntax error

--- knapsack.py
+++ knapsack.py
@@ -19,6 +19,6 @@ def knapsack(w, v, max_weight, start_item, memo):
         memo[(i - 1, max_weight - item_weight)] = value
       memo[i, max_weight] = max(memo[(i - 1, max_weight)], 
                                 memo[(i - 1, max_weight - item_weight)] + item_value)
-  return memo[(len(w) - 1, max_weight]
+  return memo[(len(w) - 1, max_weight)]
   
 print knapsack(w, v, 10, 0, {})
