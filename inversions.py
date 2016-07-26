2c98ab9b7cccdc1d3ce52c24930d5a5a8fd7dc6d
Devansh Mehta <devansh.mht@gmail.com>
2016-07-26 00:16:37 -0400

    adding comments

--- inversions.py
+++ inversions.py
@@ -12,9 +12,12 @@ def _merge_and_count_inversion(low, high, arr, tmp):
     l_pointer = mid
     r_pointer = high
     tmp_pointer = high
+    # traverse both the arrays from the end and 
+    # count number of inversions
     while l_pointer >= low and r_pointer >= mid + 1:
         if arr[l_pointer] > arr[r_pointer]:
             tmp[tmp_pointer] = arr[l_pointer]
+            # understand this carefully
             num_inversions += r_pointer - mid
             tmp_pointer -= 1
             l_pointer -= 1
@@ -38,12 +41,19 @@ def _divide_and_merge(low, high, arr, tmp):
     if low >= high:
         return 0
     mid = _get_mid(low, high)
+    # number of inversions on the left side of the array
     inversions =  _divide_and_merge(low, mid, arr, tmp)
+    # number of inversions on the right side of the array
     inversions += _divide_and_merge(mid + 1, high, arr, tmp)
+    # number of inversions after merging
     inversions += _merge_and_count_inversion(low, high, arr, tmp)
     return inversions
 
 def count_inversions(arr):
+    '''counts the number of inversion in an array
+       Uses normal merge sort algorithm to count the 
+       number of inversions. Takes O(nlog(n)) time 
+       and O(n) space complexity'''
     tmp = [-1] * len(arr)
     return _divide_and_merge(0, len(arr) - 1, arr, tmp)
 
